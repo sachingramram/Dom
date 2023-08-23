d01b748aa5c9c722aaa313299eb4c6c7ca6be1c2
Sachin Pal <sachinpal9352@gmial.com>
2023-08-23 14:58:40 +0530

    persia

---
+++ dom.js
@@ -0,0 +1,11 @@
+"use strict"; 
+
+const randomNumber1= Math.trunc(Math.random()*6 +1);
+const randomNumber2= Math.trunc(Math.random()*6 +1);
+ console.log(randomNumber1,randomNumber2);
+const image1="../html/images/D"+randomNumber1+".jpg";
+const image2="../html/images/D"+randomNumber2+".jpg";
+console.log(image2, image1);
+
+document.querySelector("#die1").setAttribute("src", image1);
+document.querySelector("#die2").setAttribute("src", image2);
